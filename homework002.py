2b8d41853214489311ff95b6fbad9f5131064275
roman <paroman.alt@gmail.com>
2025-03-07 21:37:02 +0700

    homework-2-addition исправил большую часть замечаний из комментариев к ПР

--- homework002.py
+++ homework002.py
@@ -11,84 +11,53 @@
 # ___________
 # Результат: 130
 
-
-# идея 1 - переводить в 10ичную СС, выполнять операцию и переводить результат обратно в заданную СС
-# (просто и неинтересно)
-
-# идея 2 - реализовать алгоритм как считал бы вручную, складывая в столбик (как в школе)
-
-
 from logging import exception
 from curses.ascii import isdigit
+from operator import truediv
 
-class MyException(Exception):
-    pass
+symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+           'U', 'V', 'W', 'X', 'Y', 'Z']
 
-def getCalcNumSys(num001, num002, num_cap, operation = '+'):
-    symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-               'U', 'V', 'W', 'X', 'Y', 'Z']
+def calc_addition(num1, num2, num_sys):
     result = str()
+    remaind = 0 # учет единицы в старшем разряде
+    num1_reversed = str(num1)[::-1]
+    num2_reversed = str(num2)[::-1]
+    for i in range(0, max(len(str(num1)), len(str(num2)))):
+        try:  # для случая когда числа разной длины и для уравнивания требуется добавить нули в меньшее
+            num1_symbol = num1_reversed[i]
+        except IndexError:
+            num1_symbol = '0'
+        try:
+            num2_symbol = num2_reversed[i]
+        except IndexError:
+            num2_symbol = '0'
+        index_of_sum_symbol = symbols.index(num1_symbol) + symbols.index(num2_symbol) + remaind
+        remaind = index_of_sum_symbol // num_sys
+        sum_symbol = symbols[index_of_sum_symbol % num_sys]
+        result = sum_symbol + result
+    if remaind != 0:  # для случая когда единица остается после завершения цикла (например 999 + 1)
+        result = str(remaind) + result
+    return result
 
-    # валидация ввода:
+# валидация ввода:
     # 1 - числа должны соответствовать заданной СС
     # 2 - для num001 и num002 допустим только ввод символов из списка symbols
     # 3 - для num_cap только int числа и от 2 до 36
-    # 4 - для operation только '+'
-    if type(num_cap) is not int or num_cap > 36 or num_cap < 2:
-        print('аргумент num_cap функции getCalcNumSys должен быть целым числом от 2 до 36')
-        raise MyException(Exception)
-    for digit in str(num001) + str(num002):
-        if digit not in symbols[0: num_cap]:
-            print('аргументы num001 и num002 функции getCalcNumSys должны состоять только из символов от 0-9 A-V '
-                  'и не должны выходить за рамки заданной СС')
-            raise MyException(Exception)
-    if operation not in ['+']:
-        print('аргумент operation функции getCalcNumSys должен быть одним из символов: "+"')
-        raise MyException(Exception)
-
-    match operation:
-        case '+':
-            remaind = 0 # учет единицы в старшем разряде
-            for i in range(0, max(len(str(num001)), len(str(num002)))):
-                try: # для случая когда числа разной длины и для уравнивания требуется добавить нули в меньшее
-                    num001_digit = str(num001)[::-1][i]
-                except IndexError:
-                    num001_digit = '0'
-                try:
-                    num002_digit = str(num002)[::-1][i]
-                except IndexError:
-                    num002_digit = '0'
-                indx = symbols.index(num001_digit) + symbols.index(num002_digit) + remaind
-                if indx >= num_cap:
-                    remaind = 1
-                    indx = indx % len(symbols) # на случай если индекс суммарного символа выходит за список
-                    sum_digits = symbols[indx % num_cap] # остаток, если суммарный символ больше разрядности СС
-                else:
-                    remaind = 0
-                    sum_digits = symbols[indx]
-                result = sum_digits + result
-            if remaind != 0: # для случая когда единица остается после завершения цикла (например 999 + 1)
-                result = str(remaind) + result
-            return result
-
-# как тестировал:
-# getCalcNumSys(111, '32D', 16)   -------> 43E
-# getCalcNumSys('0', '0', 2)      -------> 0
-# getCalcNumSys(22, 33, 10)       -------> 55
-# getCalcNumSys('999', '1', 10)   -------> 1000
-# getCalcNumSys('999', '1', 12)   -------> 99A
-# getCalcNumSys('AAA', 'BBB', 16) -------> 1665
-# etCalcNumSys('AVA', 'BVB', 36)  -------> MQL
-# getCalcNumSys('Z', '1', 36)     -------> 10
-# getCalcNumSys('Z', 'Z', 36)     -------> 1Y
-# getCalcNumSys('Z', -1, 36)      -------> ошибка (предусмотренная)
-# getCalcNumSys('Z', 'Z', 36, 'plus') ---> ошибка
-# getCalcNumSys('Z', 'Z', 37)     -------> ошибка
-# getCalcNumSys('Z', 'Z', '36')   -------> ошибка
-# getCalcNumSys('999', '1', 2)    -------> ошибка
-# getCalcNumSys('9+=?9', '13', 10) ------> ошибка
+    # 4 - для operation только "+", "-", "/", "*"
+def calc_validation(num_sys, num = None, operation = None): # возвращает true если валидно и false если невалидно
+    if type(num_sys) is not int or num_sys > 36 or num_sys < 2: # Основание СС должно быть целым числом от 2 до 36
+        return False
+    if num != None: # для того чтобы эту функцию можно было применить после ввода пользователем СС
+        for digit in str(num):
+            if digit not in symbols[0: num_sys]: # Число должно состоять только из символов 0-9 и A-V и не должно выходить за рамки заданной СС
+                return False
+    if operation != None and operation not in ["+", "-", "/", "*"]:
+        return False
+    return True
 
 
-print(getCalcNumSys('A', '-1', 16))
+# print(calc_addition('8', '1', 9))
+# print(calc_validation(5, 8))
